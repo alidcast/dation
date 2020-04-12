5c8dbde4ac79000ea82e823bbfa278595fe3f33f
Alid <alidcastano@gmail.com>
2020-04-12 10:08:45 -0500

    made #attr and #ent literals distinct; added #spec and #enum

--- src/rejure/dation/schema.clj
+++ src/rejure/dation/schema.clj
@@ -1,40 +1,65 @@
 (ns rejure.dation.schema "Define Datomic Schemas with EDN."
   (:require [clojure.edn :as edn]))
 
-(defn- ent-vec->datomic-attr-map "Converts entity vector attribute shorthand to datomic attribute map."
+(defn- attr->datomic-attr-map
+  "Converts generic attribute shorthand to datomic attribute map.
+   Excepts vector `v` of [ident type cardinality (?unique | ?component) ?pred]"
   [v]
-  (if (>= (count v) 3)
+  (if-not (and (vector? v) (>= (count v) 3))
+    (throw (AssertionError. "#attr declaration must be vector with min of ident, type, and cardinality attrs."))
     (let [[ident type cardinality] v
-          unique (get v 3)
+          constraint (get v 3)
+          unique     (when (keyword? constraint) constraint)
+          comp?      (when (boolean? constraint) constraint)
+          pred       (get v 4)
           attrs  (cond->> {:db.ident       ident
                            :db.type        type
                            :db.cardinality cardinality}
-                   (some? unique) (merge {:db.unique unique}))]
-      attrs)
-    (throw (AssertionError. "Entity vector must have ident, type, and cardinality attributes declared."))))
+                   (some? unique) (merge {:db.unique unique})
+                   (some? comp?)  (merge {:db.isComponent comp?})
+                   (some? pred)   (merge {:db.attr/preds pred}))]
+      attrs)))
 
-;; TODO handle references vs regular attributes (unique vs isComponent)
 (defn- ent->datomic-attr-map 
-  "Convert entity shorthand `x` to datomic attribute map.
-   Declaration can be a vector of form '[ident type cardinality ?unique].
-   Or it can be a map of form '{ident attrs} where attrs can be a map or above vector shorthand, 
-   and the ident key will be merged with the final attrs."
-  [x]
-  (cond
-    (vector? x) (ent-vec->datomic-attr-map x)
-    (map? x)    (reduce (fn [acc [k v]]
-                          (if (vector? v)
-                            (conj acc (ent-vec->datomic-attr-map (into [k] v)))
-                            (conj acc (assoc v :db.ident k))))
-                        []
-                        x)
-    :else (throw (AssertionError. "Entity declaration must be a vector or a map."))))
+  "Converts entity attribute shorthand to datomic attribute map.
+   Exects map `m` of form '{ident attrs} where `attrs` is [type cardinality (?unique | ?component) ?pred]."
+  [m]
+  (if-not (map? m)
+    (throw (AssertionError. "#ent declaration must a map of {ident attrs}."))
+    (reduce (fn [acc [k v]]
+              (conj acc (if (vector? v)
+                          (attr->datomic-attr-map (into [k] v))
+                          (assoc v :db.ident k))))
+            []
+            m)))
 
+(defn- spec->datomic-attr-map
+  "Converts spec attribute shorthand to datomic attribute map.
+   Excepts vector `v` of [ident req-attrs ?pred]"
+  [v]
+  (if-not (and (vector? v) (>= (count v) 2))
+    (throw (AssertionError. "#spec declaration must be vector with ident and req-attrs."))
+    (let [[ident req-attrs] v
+          pred  (get v 2)
+          attrs  (cond->> {:db.ident        ident
+                           :db.entity/attrs req-attrs}
+                   (some? pred)   (merge {:db.attr/preds pred}))]
+      attrs)))
 
-(defn create-readers "Create edn reader literals."
+(defn- enum->datomic-attr-map
+  [kw]
+  (if-not (keyword? kw)
+    (throw (AssertionError. "#enum declaration must be a keyword."))
+    {:db/ident kw}))
+
+(defn create-readers "Create edn reader literal attribute shorthands."
   []
-  {'ent ent->datomic-attr-map})
+  {'attr attr->datomic-attr-map
+   'ent  ent->datomic-attr-map
+   'spec spec->datomic-attr-map
+   'enum enum->datomic-attr-map})
 
+;; TODO consider where installs should be ascending or descending, or even in place with version numbers.
 (defn read-edn "Reads schema as edn string `s`."
   [s] (edn/read-string {:readers (create-readers)}
                        s))
--- test/rejure/dation/schema_test.clj
+++ test/rejure/dation/schema_test.clj
@@ -5,18 +5,19 @@
 (deftest reader-edn-test
   (let [read-edn dschema/read-edn]
     (testing "reads edn literals correctly"
-      (testing "#ent converts shorthand vector to datomic attribute map"
+      (testing "#attr converts shorthand vector to datomic attribute map"
         (testing "form: [ident type cardinality]"
-          (is (= (read-edn "[#ent [:user/username :db.type/string :db.cardinality/one]]")
+          (is (= (read-edn "[#attr [:user/username :db.type/string :db.cardinality/one]]")
                  [{:db.ident       :user/username
                    :db.type        :db.type/string
                    :db.cardinality :db.cardinality/one}])))
         (testing "form: [ident type cardinality unique]"
-          (is (= (read-edn "[#ent [:user/username :db.type/string :db.cardinality/one :db.unique/identity]]")
+          (is (= (read-edn "[#attr [:user/username :db.type/string :db.cardinality/one :db.unique/identity]]")
                  [{:db.ident       :user/username
                    :db.type        :db.type/string
                    :db.cardinality :db.cardinality/one
-                   :db.unique      :db.unique/identity}])))
+                   :db.unique      :db.unique/identity}]))))
+      (testing "#ent converts shorthand vector to datomic attribute map"
         (testing "form: {ident attrs-vector}"
           (is (= (read-edn "[#ent #:user{:username [:db.type/string :db.cardinality/one]}]")
                  [[{:db.ident       :user/username
