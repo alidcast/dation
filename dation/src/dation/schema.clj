bba3b42f395c98143abbe85c2b28ee80fec2b9ca
Alid <alidcastano@gmail.com>
2020-06-21 17:06:18 -0500

    add doc and optional pred to #db/spec

--- dation/src/dation/schema.clj
+++ dation/src/dation/schema.clj
@@ -1,12 +1,6 @@
-(ns dation.schema
-  "Datomic schema accretion tools."
+(ns dation.schema "Define Datomic schema attributes in EDN."
   (:require [clojure.edn :as edn]
-            [clojure.string :as str]
-            [datomic.client.api :as d]
             [clojure.spec.alpha :as s]))
-
-;; # Config Reader Literals
-;; Edn literal shortshands for defining Datomic schema attributes.
 
 ;; == #db/attr == 
 
@@ -54,28 +48,31 @@
 
 (s/def ::spec
   (s/cat :ident  keyword?
+         :doc    (s/? string?)
          :req-attrs (s/coll-of keyword?)
          :preds (s/? ::attr-preds)))
 
 (comment
   (s/conform ::spec [:user.spec/new [:user/email] 'foo]))
 
+
 (defn- spec->datomic-attr-map
   "Converts spec attribute shorthand to datomic attribute map.
    Excepts vector `v` of [ident req-attrs ?pred]"
   [v]
-  (let [parsed (s/conform ::attr v)]
+  (let [parsed (s/conform ::spec v)]
+    (println parsed)
     (if (= parsed ::s/invalid)
-      (throw (ex-info "Invalid #db/spec input." (s/explain-data ::attr parsed)))
-      (let [{:keys [ident req-attrs preds]} parsed
-            pred  (get v 2)
-            attrs  (cond-> {:db/ident        ident
-                            :db.entity/attrs req-attrs}
-                     (some? pred) (assoc :db.attr/preds preds))]
-        attrs))))
+      (throw (ex-info "Invalid #db/spec input." (s/explain-data ::spec parsed)))
+      (let [{:keys [ident doc req-attrs preds]} parsed]
+        (cond-> {:db/ident        ident
+                 :db/doc         doc
+                 :db.entity/attrs req-attrs}
+          (some? preds) (assoc :db.attr/preds preds))))))
 
-(comment 
- (spec->datomic-attr-map [:user.spec/new [:user/username] 'db.fns/new-user?]))
+(comment
+  (spec->datomic-attr-map [:user.spec/new [:user/username]])
+  (spec->datomic-attr-map [:user.spec/new [:user/username] 'db.fns/new-user?]))
 
 ;; == #db/enum == 
 
@@ -102,51 +99,3 @@
       :migrations  List of data migrations to run."
   [s] (edn/read-string {:readers (create-readers)}
                        s))
-
-;; # Attribute Installer
-;; Ensures that given schema attributes are install in database.
-
-(defn has-attr? "Checks if `db` has attribute by `ident`."
-  [db ident]
-  (-> (d/pull db {:selector '[:db/valueType]
-                  :eid ident})
-      seq
-      boolean))
-
-
-(defn ensure-admin-attrs "Ensures that attributes used for tracking schema accretions are installed."
-  [conn]
-  (when-not (has-attr? (d/db conn) :dation.schema/install)
-    (d/transact conn {:tx-data (attr->datomic-attr-map
-                                [:dation.schema/name "Schema name used by Dation to track attribute installs."
-                                 [:db.type/keyword :db.cardinality/one]])})))
-
-(defn installed? "Checks if `db` has attributes of schema name `sn` installed."
-  [db sn]
-  (and (-> (d/q {:query '[:find ?e
-                          :in $ ?sn
-                          :where [?e :dation.schema/name ?sn]]
-                 :args [db sn]})
-           seq
-           boolean)))
-
-(defn attrs "Gets map of all installed `db` schema attributes."
-  [db]
-  (->> (d/pull db '{:eid 0 :selector [{:db.install/attribute [*]}]})
-       :db.install/attribute
-       (remove (fn [m] (str/starts-with? (namespace (:db/ident m)) "db")))
-       (map #(update % :db/valueType :db/ident))
-       (map #(update % :db/cardinality :db/ident))))
-
-(defn ensure-ready
-  "Ensure that `schema` migrations have run (TODO) and attributes have been installed.
-   See [[read-edn]] for configuration details."
-  [conn schema]
-  (ensure-admin-attrs conn)
-  (let [name (:name schema)]
-    ;; todo: installing based on name only bc we removed version.. 
-    (when-not (installed? (d/db conn) name)
-      (doseq [attrs (:installs schema)]
-        (d/transact conn {:tx-data (cons {:dation.schema/name name}
-                                         attrs)})))
-    :ready))
